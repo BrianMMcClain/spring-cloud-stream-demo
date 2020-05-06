b8aa29c6a5af350ee05769bb8f968fd27d97a7c3
Brian McClain <brianmmcclain@gmail.com>
2020-05-06 12:29:17 -0400

    Update to latest version of Spring and Spring Cloud Stream, match output channel

--- loancheck/src/main/java/io/pivotal/loancheck/LoanProcessor.java
+++ loancheck/src/main/java/io/pivotal/loancheck/LoanProcessor.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public interface LoanProcessor {
 
-  String APPLICATIONS_IN = "output";
+  String APPLICATIONS_IN = "supplyLoan-out-0";
   String APPROVED_OUT = "approved";
   String DECLINED_OUT = "declined";
 
